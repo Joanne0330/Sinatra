1f2e86a2b12300e6d6ba3cc7ea86d1be7d376256
Joanne Chen <joanne_r_chen@yahoo.com>
2020-06-22 16:45:54 +0100

    Added kitten photo with red dotted border

--- app.rb
+++ app.rb
@@ -8,3 +8,9 @@ end
 get '/secret' do
   "We have a secret, but we're not going to tell you... you'll never guess what it is!"
 end
+
+get '/cat' do
+  "<div style= 'border: 3px dashed red'>
+    <img src='http://bit.ly/1eze8aE'>
+  </div>"
+end
