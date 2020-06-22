cde8b7702113948df22e41c20f2d3f837a28c519
India Rex <face940@gmail.com>
2020-06-22 16:24:32 +0100

    add session secret setter

--- app.rb
+++ app.rb
@@ -1,9 +1,10 @@
 require 'sinatra'
+set :session_secret, 'super secret'
 
 get '/' do
-  "Hello World, it's Joanne and India"
+  "Hello World, it's Joanne and India, here to take over the world"
 end
 
 get '/secret' do
-  "We have a secret"
+  "We have a secret, but we're not going to tell you... you'll never guess what it is!"
 end
